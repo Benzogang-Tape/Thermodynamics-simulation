903fe7bf0d145e68799427c7f28ca7fe4e6721df
DJ Tape <Dj.Tapee@yandex.ru>
2024-05-01 23:56:17 +0300

    Add reactor entity

---
+++ Reactor.cpp
@@ -0,0 +1,8 @@
+//
+// Created by DJ Tape on 01.05.2024.
+//
+
+#include "Reactor.hpp"
+
+namespace Reactor {
+} // Reactor
---
+++ Reactor.hpp
@@ -0,0 +1,16 @@
+//
+// Created by DJ Tape on 01.05.2024.
+//
+
+#ifndef THERMODYNAMICS_SIMULATION_REACTOR_HPP
+#define THERMODYNAMICS_SIMULATION_REACTOR_HPP
+
+namespace Reactor {
+
+	class Reactor {
+
+	};
+
+} // Reactor
+
+#endif //THERMODYNAMICS_SIMULATION_REACTOR_HPP
